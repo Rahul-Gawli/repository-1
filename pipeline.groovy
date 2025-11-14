e8faf028a3b31ab694883272066bac6f7ea4f351
Rahul-Gawli <rahulgawli789@gmail.com>
2025-11-14 18:56:44 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,34 @@
+pipeline{
+    agent {label 'slave'}
+    
+    
+    
+    stages {
+        stage ('git_checkout'){
+            steps{
+                git branch: 'main', url: 'https://github.com/Rahul-Gawli/student-ui-app.git'
+            }
+        
+        
+        
+        stage ('build-stage'){
+            steps{
+               sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+        
+        
+        
+        stage('test-stage'){
+             steps{
+                echo "test-success"
+             }
+        }
+        stage (deploy-stage){
+            steps{
+                echo "deploy-success"
+             }
+            }
+        }
+    }
+}
