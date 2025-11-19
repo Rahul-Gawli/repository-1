1136f44d36b52b74c1341d2a1b00e08bfbb2f5b6
Rahul-Gawli <rahulgawli789@gmail.com>
2025-11-19 16:37:45 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,48 +1,23 @@
-pipeline{
+pipeline {
     agent {label 'slave'}
-    
-     
-    
     stages {
-        
-    
-        stage ('git_checkout') {
-            steps{
+        stage('git_checkout') {
+            steps {
                 git branch: 'main', url: 'https://github.com/Rahul-Gawli/student-ui-app.git'
             }
         }
-        
-        
-        stage ('build-stage') {
-            steps{
+        stage('build-stage') {
+            steps {
                sh '/opt/maven/bin/mvn clean package'
+             
             }
         }
-        
-        
-        
         stage('test-stage') {
-             steps{
-                    // sh '''/opt/maven/bin/mvn sonar:sonar  -Dsonar.projectKey=student-app  -Dsonar.host.url=http://52.87.174.235:9000  -Dsonar.login=ce05780eaa062933a4b17c76ba4bbaae7ee0b974'''
-                    // withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
-                           // sh '/opt/maven/bin/mvn sonar:sonar'
-                 echo "test successful"
-                }
-             }
-        }
-
-        // stage('quality-gate') {
-        //       steps{
-        //               timeout(10) {
-        //             }
-        //               waitForQualityGate true
-        //       }   
-        // }
-        
-        stage ('deploy-stage') {
-            steps{
-                echo "deploy-success"
-             }
+            steps {
+               withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
+                    sh '/opt/maven/bin/mvn sonar:sonar' 
+            }
+          }
         }
     }
 }
